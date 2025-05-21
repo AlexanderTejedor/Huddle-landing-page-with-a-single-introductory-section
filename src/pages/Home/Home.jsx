62dbb10b3e6cf5d31879b67a78d80b9c4ada3bc9
Alexander Tejedor <atromero.98@gmail.com>
2025-05-20 21:51:12 -0500

    feat(responsive): Complete full responsive design for mobile and desktop

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,26 +4,27 @@ import bgMov from '../../assets/img/bg-mobile.svg'
 import bgDes from '../../assets/img/bg-desktop.svg'
 
 const  Home = () => {
+    const hiperValues = 'w-8 h-8 border-white border-1 rounded-full flex items-center justify-center hover:text-soft-magenta hover:border-soft-magenta';
     return (
         <main className=''>
             <img className='-z-1 absolute w-screen md:hidden' src={bgMov} alt="Mobile background illustration"/>
-            <img className='hidden md:absolute md:w-screen md:object-contain md:block md:-z-1' src={bgDes} alt="" />
-            <section className='px-8'>
-                <img className='w-32 pt-8 pb-15' src={logo} alt="Huddle logo" />
-                <section className=''>
-                    <figure>
-                        <img src={bannerImg} alt="Illustration of mockup screens" />
+            <img className='hidden md:absolute md:w-screen md:h-screen md:object-cover md:block md:-z-1' src={bgDes} alt="" />
+            <section className='px-8 lg:h-screen lg:flex lg:flex-col lg:justify-between lg:px-15'>
+                <img className='w-32 pt-8 pb-15 md:w-40 lg:w-50 lg:pt-15 lg:pb-0' src={logo} alt="Huddle logo" />
+                <section className='lg:flex lg:items-center lg:justify-center lg:gap-8 lg:max-w-300 lg:self-center xl:max-w-400 xl:gap-15'>
+                    <figure className='w-full flex justify-center items-center'>
+                        <img className='w-100 xl:min-w-200' src={bannerImg} alt="Illustration of mockup screens" />
                     </figure>
-                    <section className='mt-13 text-white text-center'>
-                        <h1 className='text-2xl/9 font-semibold mb-5 font-title'>Build The Community Your Fans Will Love</h1>
-                        <p className='font-display px-1 pb-6'>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
-                        <button className='cursor-pointer text-xs bg-white text-violet px-19 py-3 rounded-full font-light shadow-[2px_8px_11px_0px_rgba(51,_65,_85,_0.12)] hover:bg-soft-magenta hover:text-white'>Register</button>
+                    <section className='mt-13 text-white text-center lg:text-left lg:200 xl:mr-20'>
+                        <h1 className='text-2xl/9 font-semibold mb-5 font-title lg:text-4xl/12 lg:font-medium xl:text-5xl/16 xl:font-medium'>Build The Community Your Fans Will Love</h1>
+                        <p className='font-display px-1 pb-6 md:px-25 lg:px-0 xl:text-2xl'>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+                        <button className='cursor-pointer text-xs bg-white text-violet px-19 py-3 rounded-full font-light shadow-[2px_8px_11px_0px_rgba(51,_65,_85,_0.12)] hover:bg-soft-magenta hover:text-white lg:px-13 lg:text-base'>Register</button>
                     </section>
                 </section>
-                <section className='mt-15 flex items-center justify-center gap-3 text-white'>
-                    <a className='w-8 h-8 border-white border-1 rounded-full flex items-center justify-center hover:text-soft-magenta hover:border-soft-magenta' href="#"><i className="ri-facebook-fill"></i></a>
-                    <a className='w-8 h-8 border-white border-1 rounded-full flex items-center justify-center hover:text-soft-magenta hover:border-soft-magenta' href="#"><i className="ri-twitter-fill"></i></a>
-                    <a className='w-8 h-8 border-white border-1 rounded-full flex items-center justify-center hover:text-soft-magenta hover:border-soft-magenta' href="#"><i className="ri-instagram-line"></i></a>
+                <section className='mt-15 flex items-center justify-center gap-3 text-white lg:justify-end lg:mb-10'>
+                    <a className={hiperValues} href="#"><i className="ri-facebook-fill"></i></a>
+                    <a className={hiperValues} href="#"><i className="ri-twitter-fill"></i></a>
+                    <a className={hiperValues} href="#"><i className="ri-instagram-line"></i></a>
                 </section>
             </section>
         </main>
